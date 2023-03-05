b0d76a62e3d6c432f1189b1f3083dbf710840ac5
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2023-03-05 09:45:20 -0800

    Chapter 5, first try

---
+++ ch5/report.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include "grade.h"
+#include "Student_info.h"
+
+using std::cin;             using std::setprecision;
+using std::cout;            using std::sort;
+using std::domain_error;    using std::streamsize;
+using std::endl;            using std::string;
+using std::max;             using std::vector;
+
+int main() {
+    vector<Student_info> students;
+    Student_info record;
+    string::size_type maxlen = 0;
+
+    // read and store all the records and find the length of the longest name
+    // Invariant: students contains all the student records read so far
+    //              maxlen contains the length of the longest name in students
+    while (read(cin, record)) {
+        maxlen = max(maxlen, record.name.size());
+        students.push_back(record);
+    }
+
+    vector<Student_info> failing_students =
+        extract_fails(students);
+
+    // alphabetize the records
+    sort(students.begin(), students.end(), compare);
+
+    
+    // write the names and grades
+    for (vector<Student_info>::size_type i = 0;
+            i != students.size(); ++i) {
+
+        // write the name, padded on the right to maxlen + 1 characters
+        cout << students[i].name
+            << string(maxlen + 1 - students[i].name.size(), ' ');
+
+        // compute and write the grade
+        try {
+            double final_grade = grade(students[i]);
+            streamsize prec = cout.precision();
+            cout << setprecision(3) << final_grade
+                << setprecision(prec);
+        } catch (domain_error e) {
+            cout << e.what();
+        }
+        cout << endl;
+    }
+    return 0; 
+}
